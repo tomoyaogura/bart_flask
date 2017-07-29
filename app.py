d254fc5ab08cb6dd61a88e28df4216e1ad039e67
Tommy Ogura <tommyo@captricity.com>
2017-07-29 00:01:12 -0700

    Javascript dev

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request, make_response
 from flask_wtf import Form
 from flask_bootstrap import Bootstrap
 
 from bart_api import bart_api
 from tokens import BART_TOKEN
 from python_nextbus.nextbus import api
+from json import dumps
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -20,24 +21,32 @@ else:
 def bus_departures():
     departures = api.predictions(agency='actransit', route=route, s=stop_id)
     if departures:
-        return departues
+        return departures
     else:
         return ['No departure estimate']
 
 def bart_departures(station):
     info = bart.etd(station)
-    lines = {}
+    departures = []
     for dest in info:
-        dest['destination']
-        departures = []
+        line = {'destination': dest['destination']}
+        estimates = []
         for est in dest['estimates']:
             if est['minutes'] == 'Leaving':
                 msg = '{} car train leaving now'.format(est['length'])
             else:
                 msg = '{} car train leaving in {} minutes'.format(est['length'], est['minutes'])
-            departures.append(msg)
-        lines[dest['destination']] = departures
-    return lines
+            estimates.append(msg)
+        line['estimates'] = estimates
+        departures.append(line)
+    return departures
+
+@app.route('/_add_numbers')
+def add_numbers():
+    a = request.args.get('a', 0, type=int)
+    b = request.args.get('b', 0, type=int)
+    result = bart_departures('19TH')
+    return jsonify(departures=bart_departures('19TH'))
 
 @app.route("/")
 def index_page():
